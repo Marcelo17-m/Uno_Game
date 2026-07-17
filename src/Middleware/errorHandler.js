344571d9e2f532044db591b083533f7e17fb3015
Marcelo_Medrano <marcelo.medrano0125@jala.university>
2026-07-17 10:18:22 -0500

    Update the middleware to add a errorHandler and a AppError to make the erros more easy to represent in the service and everywhere else

--- src/Middleware/errorHandler.js
+++ src/Middleware/errorHandler.js
@@ -1,12 +1,23 @@
-function errorHandler(err, req, res, next) {
-    console.error(err);
+class AppError extends Error {
+    constructor(message, statusCode = 500) {
+        super(message);
+        this.statusCode = statusCode;
+        this.isOperational = true;
+    }
+}
 
+const errorHandler = (err, req, res, next) => {
     const statusCode = err.statusCode || 500;
+
+    if(!err.isOperational){
+        console.error(err);
+    }
+
     const message = err.message || 'Internal Server Error';
 
     res.status(statusCode).json({
         error: message
     })
-}
+};
 
-export default errorHandler;
+export { AppError, errorHandler };
